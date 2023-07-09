945365e9232c14a28b97c4588abbcd0a56d7404d
masood233899 <127298106+masood233899@users.noreply.github.com>
2023-07-09 23:16:57 +0530

    Add files via upload

---
+++ ADO/DbEfApi/Services/Emp_Services/IEmp.cs
@@ -0,0 +1,14 @@
+﻿using DbEfApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbEfApi.Services.Emp_Services
+{
+    public interface IEmp
+    {
+        Task<List<Emp>> GetEmps();
+        Task<Emp?> GetEmp(int id);
+        Task<Emp> PutEmp(int id, Emp emp);
+        Task<Emp> PostEmp(Emp emps);
+        Task<string> DeleteEmp(int id);
+    }
+}
